317ba09fd8401fe3e4ecbd370870e57661ed4f6a
asow123@gmail.com <asow123@gmail.com>
2014-06-04 18:48:10 -0400

    DialPad

---
+++ IMDialViewController.h
@@ -0,0 +1,13 @@
+//
+//  IMDialViewController.h
+//  EsVerta LLC
+//
+//  Created by Andrew Sowers on 6/4/14.
+//  Copyright (c) 2014 Andrew Sowers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IMDialViewController : UIViewController
+
+@end
---
+++ IMDialViewController.m
@@ -0,0 +1,49 @@
+//
+//  IMDialViewController.m
+//  EsVerta LLC
+//
+//  Created by Andrew Sowers on 6/4/14.
+//  Copyright (c) 2014 Andrew Sowers. All rights reserved.
+//
+
+#import "IMDialViewController.h"
+
+@interface IMDialViewController ()
+
+@end
+
+@implementation IMDialViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
